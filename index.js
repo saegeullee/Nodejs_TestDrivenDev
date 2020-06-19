19be56262f6fdd821a0b8fd4b4361936b41cea93
saegeullee <lemix777@naver.com>
2020-06-19 19:07:01 +0900

    Implemented /users endpoint testing

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const port = 3001;
 
 let users = [
   { id: 1, name: 'alice' },
-  { id: 1, name: 'alice' },
-  { id: 1, name: 'alice' }
+  { id: 2, name: 'hyuna' },
+  { id: 3, name: 'louies' }
 ];
 
 app.use(morgan('dev'));
-app.get('/users', (req, res) => res.json(users));
+app.get('/users', (req, res) => {
+  req.query.limit = req.query.limit || 10;
+  let limit = Number(req.query.limit);
+  console.log(typeof limit);
+
+  if (isNaN(limit) || typeof limit !== 'number') {
+    return res.status(400).end();
+  }
+
+  res.json(users.slice(0, limit));
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+module.exports = app;
---
+++ index.spec.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const should = require('should');
+const app = require('./index');
+
+describe('GET /users는', () => {
+  describe('성공시', () => {
+    it('user 객체를 담은 배열로 응답한다.', done => {
+      request(app)
+        .get('/users')
+        .end((err, res) => {
+          res.body.should.be.instanceOf(Array);
+          console.log(res.body);
+          done();
+        });
+    });
+    it('최대 limit 갯수만큼 응답한다.', done => {
+      request(app)
+        .get('/users?limit=2')
+        .end((err, res) => {
+          res.body.should.have.lengthOf(2);
+          done();
+        });
+    });
+  });
+
+  describe('실패시', () => {
+    it('limit이 숫자형이 아니면 400을 응답한다.', done => {
+      request(app)
+        .get('/users?limit=two')
+        .expect(400)
+        .end(done);
+    });
+  });
+});
